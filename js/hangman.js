5013a1e2a5b4e4c13a30fd3c77c1b34969e49e30
Sandro Soncini <sandrosoncini@hotmail.com>
2020-01-29 21:13:53 -0800

    game functionality

--- js/hangman.js
+++ js/hangman.js
@@ -11,33 +11,107 @@ const programmingLanguages = [
 ]
 
 let answer = '';
-let maxWrong = 6;
 let mistake = 0;
 let guessed = [];
+let wordStatus = null;
+let maxWrong = 6;
+document.getElementById('maxWrong').innerHTML = maxWrong;
+
+
+
 
 function randomWord(){
-    answer = programmingLanguages[Math.floor(Math.random () * programmingLanguages.length)];
-    //alert(answer)
-}
+     answer = programmingLanguages[Math.floor(Math.random () * programmingLanguages.length)];
+};
 
 function generateButtons(){
-    let buttonHtml = 'abcdefghijklmnopqrstuvwxyz'.split("")
-//     you have an array of abcdefg 
-// 1) looop through
-buttonHtml.forEach(letter => {
-    const button = dom.createElement('<div class="letter">letter</div>')
-})
-// 2) you need to get keycode of every character (optional)
-// 3) const a = dom.createElement('<div>`${letter}`</div>')
-// 4) add an event to the element you created
-// 5) on click it will check the word and see if it has this character
-// 6) if it does display that character
-// 7) if not increment mistake
+    let letterArray = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.toLowerCase().split("");
+
+    const buttonsHtml = letterArray.map(letter =>
+        `
+        <button
+            class="key"
+            id='`+ letter +`'
+            onClick="handleGuess('`+ letter +`')"
+        >
+            `+ letter +`
+        </button>
+        `).join(" ");
+
+    document.getElementById("keyboard").innerHTML = buttonsHtml
+}; 
+
+function handleGuess(letter){
+    
+    guessed.indexOf(letter) === -1 ? guessed.push(letter) : null;
+    document.getElementById(letter).setAttribute('disabled', true);
+
+    if(answer.indexOf(letter) >= 0){
+        guessedWords()
+        gameWin()
+    }else if (answer.indexOf(letter) === -1) {
+        mistake += 1
+        updateMistake()
+        changePic()
+        gameLost()
+    };
+};
+
+function gameWin(){
+    if(wordStatus === answer){
+        document.getElementById('keyboard').innerHTML = "YOU WIN MY LOVE"
+    };
+};
+
+function gameLost(){
+    if(mistake === maxWrong){
+        document.getElementById('keyboard').innerHTML = "YOU LOST MTF"
+    };
+};
+
+function guessedWords(){
+    wordStatus = answer.split("").map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join("");
+    document.getElementById("wordSpotlight").innerHTML = wordStatus
+};
+
+function updateMistake(){
+    document.getElementById('mistakes').innerHTML = mistake;
+};
+    
 
 
-return button
-}
+function changePic(){
+    if(mistake === 1){
+        document.getElementById('hangmanImage').src = "./images/gallows+head.jpg"
+    } else if(mistake === 2){
+        document.getElementById('hangmanImage').src = "./images/gallows+head+torso.jpg"
+    } else if(mistake === 3){
+        document.getElementById('hangmanImage').src = "./images/gallows+head+torso+arm.jpg"
+    } else if(mistake === 4){
+        document.getElementById('hangmanImage').src = "./images/gallows+head+torso+2leg.jpg"
+    } else if(mistake === 5){
+        document.getElementById('hangmanImage').src = "./images/gallows+head+torso+2leg+arm.jpg"
+    }else if(mistake === 6){
+        document.getElementById('hangmanImage').src = "./images/gallows+head+torso+2leg+2arm.jpg"
+    }else{
+        null;
+    };
+};
 
-generateButtons()
+function reset(){
+    mistake = 0;
+    guessed = [];
+    document.getElementById('hangmanImage').src = "./images/gallows.jpg"
+   
+    generateButtons();
+    randomWord();
+    guessedWords();
+    updateMistake();
+};
+
+
+generateButtons();
 randomWord();
+guessedWords();
 
+ 
